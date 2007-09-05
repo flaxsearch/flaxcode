db0000103c6d17429d15830b3aea8cdd753bd585
paul.x.rudin <paul.x.rudin@07824224-6132-0410-9051-db7a3662f6e8>
2007-09-05 10:22:14 +0000

    reinstated code inadvertenly removed during rename.

--- src/test/testfilespec.py
+++ src/test/testfilespec.py
@@ -1,4 +1,34 @@
+from __future__ import with_statement
 import unittest
+import os
+import shutil
+import sys
+sys.path = [os.path.normpath(os.path.join(__file__, '..', '..'))]+sys.path
+import filespec
 
-class FileSpecTestCase(unittest.TestCase):
-    pass
+
+class TestFileSpec(unittest.TestCase):
+
+    dirname = "/tmp/testfilespecdir/"
+
+    def setUp(self):
+        subdirs = ['foo', 'bar']
+        if os.path.exists(self.dirname):
+            shutil.rmtree(self.dirname)
+        os.mkdir(self.dirname)
+        for dir in subdirs:
+            dname = os.path.join(self.dirname, dir)
+            os.mkdir(dname)
+            with open(os.path.join(dname, "foo.txt"), 'w') as f:
+                f.write("Hello World")
+                        
+            
+        
+    def testbasic(self):
+        fs = filespec.FileSpec([self.dirname])
+        print list(fs.files())
+
+
+        
+if __name__ == '__main__':
+    unittest.main()
