00bf5ba817f15bebdb598d1cc3630e54328bd7b1
paul.x.rudin <paul.x.rudin@07824224-6132-0410-9051-db7a3662f6e8>
2007-09-05 03:43:43 +0000

    various changes, to at least get the iterator to work.

--- src/filespec.py
+++ src/filespec.py
@@ -1,6 +1,7 @@
 import os
 import stat
 import datetime
+import fnmatch
 
 class FileSpec(object):
     """Filespaec: define a set of files and then do things with them.
@@ -27,48 +28,84 @@ class FileSpec(object):
     FileSpec) whereas Earliest and Latest are absolute dates.
 """
 
-    def __init__(self, paths=None, exclusions=None, earliest=None, latest=None, oldest=None, youngest=None):
+    strptime_format = "%d/%m/%y %H:%M:%S"
 
-            self.paths = [os.abspath(p) for p in paths] if paths else []
+    def __init__(self, paths=None, exclusions=None, earliest=None, latest=None, oldest=None, youngest=None, formats = None):
+
+            self.paths = [os.path.abspath(p) for p in paths] if paths else []
             self.exclusions = exclusions if exclusions else []
             self.earliest = earliest
             self.latest = latest
             self.oldest = oldest
             self.youngest = youngest
+            self.formats = ['*.txt'] if formats is None else formats
 
     def files(self):
         """ generates the files defined by this FileSpec """
         for p in self.paths:
-            for root, dir, files in os.walk(p):
+            for root, dirs, files in os.walk(p):
                 for f in files:
-                    if self.included(f):
-                        yield f
+                    fname = os.path.join(root, f)
+                    if self.included(fname):
+                        yield fname
+                        
+#                dirs = filter(dirs, self.dirs_included)
 
-                dirs = filter(dirs, self.dirs_included)
 
+    def _get_earliest(self):
+        return self._earliest
+
+    def _set_earliest(self, date):
+        self._earliest = self._process_datetime(date)
+
+    earliest = property(fget = _get_earliest, fset = _set_earliest)
+
+    def _process_datetime(self, date):
+        if date is None:
+            return None
+        elif type(date) is str:
+            return datetime.datetime.strptime(date, self.strptime_format)
+        elif type(date) is datetime.datetime:
+            return date
+        else:
+            raise ValueError("Value must be a None, a string, or a datetime.datetime")
+
+    def _get_latest(self):
+        return self._latest
+
+    def _set_latest(self, date):
+        self._latest = self._process_datetime(date)
+
+    latest = property(fget = _get_latest, fset = _set_latest)
+            
+    
     def included(self, f):
         """ is this file permitted?"""
 
         # is this file one of the permitted formats?
         if not any ((fnmatch.fnmatch(f, e) for e in self.formats)):
-            return false
+            return False
         # format is ok, are we with the permitted range of dates.
         mtime = datetime.datetime.fromtimestamp(os.stat(f)[stat.ST_MTIME])
         #TODO: factor out the call to now().
-        age = datetime.now() - mtime
+        age = datetime.datetime.now() - mtime
+
+        #TODO: refactor to benefit from short-ciruiting - leave like
+        #this for debugging for now
 
         # a little long-winded because we have to allow for some
         # properties being None. We could compile up a version of this
         # test each time one of the date related properties changes
         # since presumably that will happen much less often than we
         # actually use the test.
-        return not self.exclusion(f) and \
-               (self.earliest < mtime if self.earliest else True) and \
-               (mtime < self.latest if self.latest else True) and \
-               (self.oldest < age if self.oldest else True) and \
-               (age < self.youngest if self.youngest else True)
+        excluded = self.exclusion(f)
+        not_early = (self.earliest < mtime if self.earliest else True)
+        not_late = (mtime < self.latest if self.latest else True)
+        not_old = (self.oldest < age if self.oldest else True)
+        not_young = (age < self.youngest if self.youngest else True)
 
+        return (not excluded) and not_early and not_late and not_old and not_young
 
     def exclusion(self, f):
-        return not any ((fnmatch.fnmatch(f, e) for e in self.exclusions))
+        return any ((fnmatch.fnmatch(f, e) for e in self.exclusions))
                
