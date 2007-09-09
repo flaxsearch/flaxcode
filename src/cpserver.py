9821e7c8781224720cab3c1727b28dfd3e4ffac4
paul.x.rudin <paul.x.rudin@07824224-6132-0410-9051-db7a3662f6e8>
2007-09-09 06:03:12 +0000

    Made indentation consistent.
    Moved all template references to the start up code, controllers now
    take all their templates as initialization arguments.
    
    Pulled creation of controllers out of the setup_routes.

--- src/cpserver.py
+++ src/cpserver.py
@@ -1,49 +1,60 @@
+#$Id:$
+
+# Copyright (C) 2007 Lemur Consulting Ltd
+
+# This software may be used and distributed according to the terms
+# of the GNU General Public License, incorporated herein by reference.
+"""
+Flax web server.
+"""
+__docformat__ = "restructuredtext en"
 import os
 import cherrypy
 import routes
 import templates
 
 class Collections(object):
+    """
+    Controller for web pages dealing with document collections.
+    """
 
-     _template = templates.collection_list_template
-     _detail_template = templates.collection_detail_template
-
-     def __init__(self, collections):
+    def __init__(self, collections, list_template, detail_template):
          self._collections = collections
+         self._list_template = list_template
+         self._detail_template = detail_template
 
-     def do_indexing(self, col=None, **kwargs):
-         if cherrypy.request.method == "POST" and col and col in self._collections:
-             self._collections[col].make_xapian_db()
-             self.redirect_to_show(col)
-         else:
-             raise cherrypy.NotFound() 
+    def do_indexing(self, col=None, **kwargs):
+        if cherrypy.request.method == "POST" and col and col in self._collections:
+            self._collections[col].make_xapian_db()
+            self.redirect_to_show(col)
+        else:
+            raise cherrypy.NotFound() 
          
-     def update(self, col=None, **kwargs):
-         if col and col in self._collections:
-             self._collections[col].update(**kwargs)
-             self.redirect_to_show(col)
-         else:
-             raise cherrypy.NotFound() 
+    def update(self, col=None, **kwargs):
+        if col and col in self._collections:
+            self._collections[col].update(**kwargs)
+            self.redirect_to_show(col)
+        else:
+            raise cherrypy.NotFound() 
      
-     def add(self, col = None, **kwargs):
-          print kwargs
-          if col:
-               self._collections.new_collection(col, **kwargs)
-               self.redirect_to_show(col)
-          else:
-               raise cherrypy.NotFound()
+    def add(self, col = None, **kwargs):
+        if col:
+            self._collections.new_collection(col, **kwargs)
+            self.redirect_to_show(col)
+        else:
+            raise cherrypy.NotFound()
 
-     def redirect_to_show(self, col):
-          raise cherrypy.HTTPRedirect('/admin/collections/' + col + '/view' )
+    def redirect_to_show(self, col):
+        raise cherrypy.HTTPRedirect('/admin/collections/' + col + '/view' )
 
-     def view(self, col=None, **kwargs):
-          if col:
-               if col in self._collections:
-                    return self._detail_template.render(self._collections[col])
-               else:
-                    raise cherrypy.NotFound()
-          else:
-               return self._template.render(self._collections.itervalues(), routes.url_for('/admin/collections'))
+    def view(self, col=None, **kwargs):
+        if col:
+            if col in self._collections:
+                return self._detail_template.render(self._collections[col])
+            else:
+                raise cherrypy.NotFound()
+        else:
+            return self._list_template.render(self._collections.itervalues(), routes.url_for('/admin/collections'))
 
 class SearchForm(object):
 
@@ -68,60 +79,72 @@ class SearchForm(object):
          
 class Admin(object):
 
-     def __init__(self, collections):
-          self._collections = collections
-          self.admin_search = SearchForm(collections,
-                                         templates.admin_search_template,
-                                         templates.admin_search_result_template)
-     def options(self):
-          return templates.options_template.render()
+    def __init__(self, collections, search_template, search_result_template, options_template, index_template):
+        self._collections = collections
+        self._options_template = options_template
+        self._index_template = index_template
+        self._admin_search = SearchForm(collections,
+                                        search_template,
+                                        search_result_template)
 
-     def search(self, **kwargs):
-          return self.admin_search.search(advanced=False, **kwargs )
+    def options(self):
+        return self._options_template.render()
 
-     def advanced_search(self, **kwargs):
-          return self.admin_search.search(advanced=True, **kwargs )
+    def search(self, **kwargs):
+        return self._admin_search.search(advanced=False, **kwargs )
 
-     def index(self):
-          return templates.index_template.render()
+    def advanced_search(self, **kwargs):
+        return self._admin_search.search(advanced=True, **kwargs )
 
+    def index(self):
+        return self._index_template.render()
 
 class Top(object):
 
-     def __init__(self, collections):
-          self._collections = collections
-          self.user_search = SearchForm(collections,
-                                        templates.user_search_template,
-                                        templates.user_search_result_template)
+    def __init__(self, collections, search_template, search_result_template):
+        self._collections = collections
+        self._user_search = SearchForm(collections, search_template, search_result_template)
 
-     def search(self, **kwargs):
-          return self.user_search.search(advanced=False, **kwargs)
+    def search(self, **kwargs):
+        return self._user_search.search(advanced=False, **kwargs)
 
-     def advanced_search(self, **kwargs):
-          return self.user_search.search(advanced=True,  **kwargs )
+    def advanced_search(self, **kwargs):
+        return self._user_search.search(advanced=True,  **kwargs )
 
 
-def setup_routes(collections):
+def setup_routes(top_controller, admin_contoller, collections_controller):
+
     d = cherrypy.dispatch.RoutesDispatcher()
-    top = Top(collections)
-    admin = Admin(collections)
-    collections = Collections(collections)
+    d.connect('top', '/', controller = top_controller, action='search')
+    d.connect('user_search', '/search', controller = top_controller, action='search')
 
-    d.connect('top', '/', controller = top, action='search')
-    d.connect('user_search', '/search', controller = top, action='search')
+    d.connect('collections_add', 'admin/collections/add', controller = collections_controller, action='add')
+    d.connect('collections', '/admin/collections/:col/:action', controller = collections_controller, action='view')
+    d.connect('collections_default', '/admin/collections/', controller = collections_controller, action='view')
 
-    d.connect('collections_add', 'admin/collections/add', controller = collections, action='add')
-    d.connect('collections', '/admin/collections/:col/:action', controller = collections, action='view')
-    d.connect('collections_default', '/admin/collections/', controller = collections, action='view')
-
-    d.connect('admin', '/admin/:action', action='index', controller=admin )
+    d.connect('admin', '/admin/:action', action='index', controller=admin_contoller )
 
     return d
 
 
 if __name__ == "__main__":
     cd = os.path.dirname(os.path.abspath(__file__))
-    d = setup_routes(templates.COLLECTIONS)
+    collections = templates.COLLECTIONS
+
+    top = Top(collections, templates.user_search_template, templates.user_search_result_template)
+
+    admin = Admin(collections,
+                  templates.admin_search_template,
+                  templates.admin_search_result_template,
+                  templates.options_template,
+                  templates.index_template)
+    
+    collections = Collections(collections,
+                              templates.collection_list_template,
+                              templates.collection_detail_template)
+
+    d = setup_routes(top, admin, collections)
+    
     cherrypy.config.update('cp.conf')
     cherrypy.quickstart(None, config = { '/': { 'request.dispatch': d},
                                          '/static': {'tools.staticdir.on': True,
