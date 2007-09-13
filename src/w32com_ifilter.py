1305077c1b53441d99a0641502c72ae433b8420d
paul.x.rudin <paul.x.rudin@07824224-6132-0410-9051-db7a3662f6e8>
2007-09-13 11:04:22 +0000

    refactored to get rid of duplication.

--- src/w32com_ifilter.py
+++ src/w32com_ifilter.py
@@ -18,39 +18,36 @@ def prop_id_to_name(prop_id):
     else:
         return prop_id
 
-def text_for_current_chunk(f):
-    
-    def get_text():
+def gen_until_exception(callable, ex, test):
+    while True:
         try:
-            yield f.GetText()
-        except pythoncom.com_error, e:
-            if e[0] == FILTER_E_NO_MORE_TEXT:
+            yield callable()
+        except ex, e:
+            if test(e):
                 raise StopIteration
             else:
                 raise
 
-    return(''.join(get_text()))
+def text_for_current_chunk(f):
+    return(''.join( gen_until_exception(f.GetText, 
+                                        pythoncom.com_error, 
+                                        lambda e: e[0] == FILTER_E_NO_MORE_TEXT)))
 
-_filter_init_flags = IFILTER_INIT_INDEXING_ONLY | 
-                     IFILTER_INIT_APPLY_INDEX_ATTRIBUTES | 
-                     IFILTER_INIT_APPLY_CRAWL_ATTRIBUTES| 
-                     IFILTER_INIT_APPLY_OTHER_ATTRIBUTES | 
+_filter_init_flags = IFILTER_INIT_INDEXING_ONLY | \
+                     IFILTER_INIT_APPLY_INDEX_ATTRIBUTES | \
+                     IFILTER_INIT_APPLY_CRAWL_ATTRIBUTES| \
+                     IFILTER_INIT_APPLY_OTHER_ATTRIBUTES | \
                      IFILTER_INIT_SEARCH_LINKS
 
 def ifilter_filter(filename, init_flags = _filter_init_flags):
     f = ifilter.LoadIFilter(filename)
     f.Init(init_flags)
-    while True:
-        try:
-            chunk_id, break_type, flags, locale, (propset_guid, prop_id), chunk_source_id, start, len =  f.GetChunk()
-            prop_name = prop_id_to_name(prop_id)
-            if flags == CHUNK_TEXT
-                yield prop_name, text_for_current_chunk(f)
-            
-        except pythoncom.com_error, e:
-            if e[0] == FILTER_E_END_OF_CHUNKS:
-                break
-            else:
-                raise
 
+    def do_chunk():
+        chunk_id, break_type, flags, locale, (propset_guid, prop_id), chunk_source_id, start, len =  f.GetChunk()
+        prop_name = prop_id_to_name(prop_id)
+        if flags == CHUNK_TEXT:
+            return prop_name, text_for_current_chunk(f)
+        
+    return gen_until_exception(do_chunk, pythoncom.com_error, lambda e: e[0] == FILTER_E_END_OF_CHUNKS)
         
