a5af1c08dd0b2fcea467c8940fd7181474eab2f9
paul.x.rudin <paul.x.rudin@07824224-6132-0410-9051-db7a3662f6e8>
2007-09-27 08:39:19 +0000

    Got rid of the globals (except for options) and moved the intialization of a fresh options object to a function.

--- src/flax.py
+++ src/flax.py
@@ -1,72 +1,9 @@
+from __future__ import with_statement
 # standard python library imports
 import datetime
 import random
 import os
-
-
-dir = os.path.dirname(os.path.abspath(os.path.normpath(__file__)))
-user = os.path.expanduser('~')
-
-
-db_dir = os.path.normpath(dir+'/dbs') 
-
-
-log_events = ("Create Collection",
-              "Modify Collection",
-              "Delete Collection",
-              "Index Collection",
-              "Filter file",
-              "Add to doc",
-              "Remove from doc",
-              "Add to db",
-              "Remove from db",
-              "Run search",
-              "Format results")
-
-event_levels = ("None", "Critical", "Error", "Warning", "Info", "Debug", "All")
-
-default_level = event_levels[3]
-
-log_settings = dict( (e, default_level) for e in log_events)
-
-filters = ["IFilter", "Xapian", "Text"]
-
-formats = ("txt", "doc", "html")
-
-filter_settings = dict((f, filters[0]) for f in formats)
-
-languages = [ ("none",  "None"),
-              ("da",  "Danish"),
-              ("nl", "Dutch"),
-              ("en", "English"),
-              ("lovins", "English (lovins)"),
-              ("porter", "English (porter)"),
-              ("fi", "Finnish"),
-              ("fr", "French"),
-              ("de", "German"),
-              ("it", "Italian"),
-              ("no", "Norwegian"),
-              ("pt", "Portuguese"),
-              ("ru", "Russian"),
-              ("es", "Spanish"),
-              ("sv", "Swedish")]
-              
-              
-
-import flax_collections
-
-cols = flax_collections.FlaxCollections(db_dir)
-cols.new_collection("foo", 
-                    description = "foo description",
-                    paths = ["/usr/share/doc"],
-                    formats = ["txt", "doc"],
-                    indexed = datetime.date.today(),
-                    queries = random.randrange(100000),
-                    docs = random.randrange(100000),
-                    status = 0)
-
-cols.new_collection("bar",
-                    paths = ['%s/My Documents/'% user])
+import ConfigParser
 
 
 class FlaxOptions(object):
@@ -74,15 +11,10 @@ class FlaxOptions(object):
     global options for Flax
     """
     
-    # use __slots__ to ensure that we don't pass in things here that
-    # we don't expect
-    
-    __slots__ = ("collections", "db_dir", "flax_dir", "formats", "log_events", "log_levels", "log_settings", 
-                 "filters", "filter_settings", "languages")
-
     def __init__(self, collections, db_dir, flax_dir, formats, 
                  log_events, log_levels, log_settings, 
-                 filters, filter_settings, languages):
+                 filters, languages):
+        
         self.collections = collections
         self.db_dir = db_dir
         self.flax_dir = flax_dir
@@ -91,18 +23,74 @@ class FlaxOptions(object):
         self.log_levels = log_levels
         self.log_settings = log_settings
         self.filters = filters
-        self.filter_settings = filter_settings
         self.languages = languages
 
 
-options = FlaxOptions(cols, 
-                      db_dir, 
-                      dir, 
-                      formats, 
-                      log_events, 
-                      event_levels, 
-                      log_settings,
-                      filters,
-                      filter_settings,
-                      languages)
+def make_options():
+    #dir = os.path.dirname(os.path.abspath(os.path.normpath(__file__)))
+    dir = os.getcwd()
+    user = os.path.expanduser('~')
+
+
+    db_dir = os.path.normpath(dir+'/dbs') 
+
+
+    log_events = ("Create Collection",
+                  "Modify Collection",
+                  "Delete Collection",
+                  "Index Collection",
+                  "Filter file",
+                  "Add to doc",
+                  "Remove from doc",
+                  "Add to db",
+                  "Remove from db",
+                  "Run search",
+                  "Format results")
+
+    event_levels = ("None", "Critical", "Error", "Warning", "Info", "Debug", "All")
+
+    default_level = event_levels[3]
+
+    log_settings = dict( (e, default_level) for e in log_events)
+
+    filters = ["IFilter", "Xapian", "Text"]
+    
+    formats = ("txt", "doc", "html")
+
+    filter_settings = dict((f, filters[0]) for f in formats)
+
+    languages = [ ("none",  "None"),
+                  ("da",  "Danish"),
+                  ("nl", "Dutch"),
+                  ("en", "English"),
+                  ("lovins", "English (lovins)"),
+                  ("porter", "English (porter)"),
+                  ("fi", "Finnish"),
+                  ("fr", "French"),
+                  ("de", "German"),
+                  ("it", "Italian"),
+                  ("no", "Norwegian"),
+                  ("pt", "Portuguese"),
+                  ("ru", "Russian"),
+                  ("es", "Spanish"),
+                  ("sv", "Swedish")]
+              
+              
+
+    import flax_collections
+
+    cols = flax_collections.FlaxCollections(db_dir)
+
+    return FlaxOptions(cols, 
+                       db_dir, 
+                       dir, 
+                       formats, 
+                       log_events, 
+                       event_levels, 
+                       log_settings,
+                       filter_settings,
+                       languages)
+
+# placeholder for global options object
+options = None
 
