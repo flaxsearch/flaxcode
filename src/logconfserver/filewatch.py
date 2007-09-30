253dc7c790acf9ff9bffb0a089b8cc586cecb19e
paul.x.rudin <paul.x.rudin@07824224-6132-0410-9051-db7a3662f6e8>
2007-09-30 16:10:39 +0000

    logconf server, first pass

--- src/logconfserver/filewatch.py
+++ src/logconfserver/filewatch.py
@@ -1,7 +1,6 @@
 import os
 import threading
 
-
 class FileWatcher(threading.Thread):
     """ Watches a file for modification and notifies when such changes
         occur by calling a supplied function passing in the file.  We
@@ -25,19 +24,20 @@ class FileWatcher(threading.Thread):
             new_mtime = os.path.getmtime(self.filename)
             if new_mtime > self.mtime:
                 self.mtime = new_mtime
-                self.action(self.filename)
-            stop.wait(self.delay)
+                self.action()
+            self.stop.wait(self.delay)
 
 
 if __name__ == "__main__":
     import sys
     import time
-    def print_changed(filename):
+    filename = sys.argv[1]
+    def print_changed():
         print "File: %s has changed" % filename
 
     stop = threading.Event()
     stop.clear()
-    FileWatcher(stop, sys.argv[1],  print_changed, delay=1).start()
+    FileWatcher(stop,  filename, print_changed, delay=1).start()
     try:
         while True:
             time.sleep(1)
--- src/logconfserver/logconf.py
+++ src/logconfserver/logconf.py
@@ -1,45 +1,44 @@
+from __future__ import with_statement
 import collections
 import socket
 import struct
 import re
 import threading
 import ConfigParser
-
 import filewatch
 
 class LogConf(object):
     """ Simple logconfiguration control/querying
     """
 
-    def __init__(logconf_file, ports):
+    def __init__(self, logconf_file, ports):
         self.ports = ports
         self.free_ports = collections.deque(ports)
-        self.used_ports = collections.deque
+        self.used_ports = collections.deque()
         self.logconf_file = logconf_file
         self.parser = ConfigParser.SafeConfigParser()
         self.stop = threading.Event()
         self.stop.clear()
-        filewatch.FileWatcher(logconf_file, self.notify_listeners).start()
+        filewatch.FileWatcher(self.stop, self.logconf_file, self.notify_listeners).start()
 
     def __del__(self):
         self.stop.set()
-
     
-    def notify_listeners(self, filename, listeners = None):
+    def notify_listeners(self, listeners = None):
         if not listeners:
             listeners = self.used_ports
-        with open(filename) as f:
+        with open(self.logconf_file) as f:
             contents = f.read()
-            contents_len = len(contents)
+            contents_size = len(contents)
             for port in listeners:
-                self.send_to_port(contents, port, len=len)
+                self.send_to_port(contents, port, contents_size = contents_size)
 
-    def send_to_port(self, contents, port, len = None):
-        if not len:
-            len = len(contents)
+    def send_to_port(self, contents, port, contents_size = None):
+        if not contents_size:
+            contents_size = len(contents)
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect('localhost', port)
-        s.send(struct.pack('>L', len))
+        s.connect(('localhost', port))
+        s.send(struct.pack('>L', contents_size))
         s.send(contents)
         s.close()            
 
@@ -55,24 +54,20 @@ class LogConf(object):
         if port in self.used_ports:
             self.free_ports.append(port)
             self.used_ports.remove(port)
-
     
-    self.set_level(self, logger_levels):
+    def set_level(self, logger_levels):
         """
         logger_levels is a sequence of (logger, level) pairs,
         where logger is a string naming a logger and level is a number
         """
-        self.parser.read(self.filename)
+        self.parser.read(self.logconf_file)
         changed = False
         for logger, level in logger_levels:
             sec_name = 'logger_%s' % logger
-            current_level = int(sec_name, 'level'))
+            current_level = int(sec_name, 'level')
             if level != current_level:
                 self.parser.set(secname, 'level', level)
                 changed = True
         if changed:
-            with open(self.filename, 'w') as f:
+            with open(self.logconf_file, 'w') as f:
                 self.parser.write(f)
-
-            
-        
