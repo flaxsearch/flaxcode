351ed7787e7680581f4e3c22c7da27ecdb67de29
boulton.rj <boulton.rj@07824224-6132-0410-9051-db7a3662f6e8>
2007-10-05 17:57:35 +0000

    Finish packaging for 0.5

--- libs/htmltotext/setup.py
+++ libs/htmltotext/setup.py
@@ -19,42 +19,121 @@
 
 """
 
-import distutils
-from distutils.core import setup, Extension
+import sys
 
-from distutils import sysconfig
+# Use setuptools if we're part of a larger build system which is already using
+# it.
+if ('setuptools' in sys.modules):
+    import setuptools
+    from setuptools import setup, Extension
+    from setuptools.command.build_ext import build_ext
+    using_setuptools = True
+else:
+    import distutils
+    from distutils.core import setup, Extension
+    from distutils import sysconfig
+    using_setuptools = False
 
 # Customise compiler options.
-distutils_customize_compiler = sysconfig.customize_compiler
-def my_customize_compiler(compiler):
-    """Remove the -Wstrict-prototypes option from the compiler command.
-
-    This option isn't supported for C++, so we remove it to avoid annoying
-    warnings.
-
-    """
-    retval = distutils_customize_compiler(compiler)
+if using_setuptools:
     try:
-        compiler.compiler_so.remove('-Wstrict-prototypes')
-    except (AttributeError, ValueError):
+        setuptools_build_ext = build_ext.build_extension
+        def my_build_ext(self, ext):
+            """Remove the -Wstrict-prototypes option from the compiler command.
+
+            This option isn't supported for C++, so we remove it to avoid annoying
+            warnings.
+
+            """
+            try:
+                self.compiler.compiler_so.remove('-Wstrict-prototypes')
+            except (AttributeError, ValueError):
+                pass
+            retval = setuptools_build_ext(self, ext)
+            return retval
+        build_ext.build_extension = my_build_ext
+    except AttributeError:
         pass
-    return retval
-sysconfig.customize_compiler = my_customize_compiler
+else:
+    distutils_customize_compiler = sysconfig.customize_compiler
+    def my_customize_compiler(compiler):
+        """Remove the -Wstrict-prototypes option from the compiler command.
+
+        This option isn't supported for C++, so we remove it to avoid annoying
+        warnings.
+
+        """
+        retval = distutils_customize_compiler(compiler)
+        try:
+            compiler.compiler_so.remove('-Wstrict-prototypes')
+        except (AttributeError, ValueError):
+            pass
+        return retval
+    sysconfig.customize_compiler = my_customize_compiler
 
 # List of source files 
 htmltotext_sources = [
-                         'src/htmlparse.cc',
-                         'src/metaxmlparse.cc',
-                         'src/myhtmlparse.cc',
-                         'src/pyhtmltotext.cc',
-                         'src/utf8convert.cc',
-                         'src/utf8itor.cc',
-                         'src/xmlparse.cc',
-                        ]
+    'src/htmlparse.cc',
+    'src/metaxmlparse.cc',
+    'src/myhtmlparse.cc',
+    'src/pyhtmltotext.cc',
+    'src/utf8convert.cc',
+    'src/utf8itor.cc',
+    'src/xmlparse.cc',
+]
+
+# Extra arguments for setup() which we don't always want to supply.
+extra_kwargs = {}
+if using_setuptools:
+    extra_kwargs['test_suite'] = "test.test"
+
+long_description = """
+The htmltotext module
+=====================
+
+This package was written for a search engine, to allow it to extract the
+textual content and metadata from HTML pages.  It tries to cope with
+invalid markup and incorrectly specified character sets, and strips out
+HTML tags (splitting words at tags appropriately).  It also discards the
+contents of script tags and style tags.
+
+As well as text from the body of the page, it extracts the page title,
+and the content of meta description and keyword tags.  It also parses
+meta robots tags to determine whether the page should be indexed.
+
+The HTML parser used by this module was extracted from the Xapian search
+engine library (and specifically, from the omindex indexing utility in
+that library).
+
+"""
+
 
 setup(name = "htmltotext",
       version = "0.5",
+      author = "Richard Boulton",
+      author_email = "richard@lemurconsulting.com",
+      maintainer = "Richard Boulton",
+      maintainer_email = "richard@lemurconsulting.com",
+      url = "http://code.google.com/p/flaxcode/wiki/HtmlToText",
+      download_url = "http://code.google.com/p/flaxcode/downloads/list?q=htmltotext",
+      description = "Extract text and some metainfo from HTML, coping with malformed pages as well as possible.",
+      long_description = long_description,
+      classifiers = [
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: GNU General Public License (GPL)',
+          'Programming Language :: C++',
+          'Topic :: Internet :: WWW/HTTP :: Indexing/Search',
+          'Operating System :: MacOS',
+          'Operating System :: Microsoft',
+          'Operating System :: POSIX',
+      ],
+      license = 'GPL',
+      platforms = 'Any',
+
       ext_modules = [Extension("htmltotext",
                                htmltotext_sources,
                                include_dirs=['src'],
-                              )])
+                              )],
+                              
+      **extra_kwargs)
--- libs/htmltotext/src/config.h
+++ libs/htmltotext/src/config.h
@@ -1,3 +1,9 @@
+/* The values in this file are hard-coded to work on my system.
+ *
+ * FIXME - at least HAVE_SYS_ERRNO_H and HAVE_STRINGS_H should be
+ * automatically configured, somehow.
+ */
+
 /* Define the name of our package. */
 #define PACKAGE htmlparser
 
