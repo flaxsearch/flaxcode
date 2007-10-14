6e10d36a9619c2bcc7ff8a0f6928f27a770c8e19
boulton.rj <boulton.rj@07824224-6132-0410-9051-db7a3662f6e8>
2007-10-14 23:54:38 +0000

    Tidy whitespace, and rename "filename" member to "filepath", since it needs to be a full path (if we want to be independent of current working directory, as we do when running as a windows service).

--- src/logclient.py
+++ src/logclient.py
@@ -31,16 +31,16 @@ class LogListener(threading.Thread):
 
 
 class LogConf(object):
-    """ Simple logconfiguration control/querying
+    """ Simple log configuration control/querying.
+
     """
     
-    def __init__(self, filename='flaxlog.conf'):
-
-        self.filename = filename
+    def __init__(self, filepath='flaxlog.conf'):
+        self.filepath = filepath
         self.parser = ConfigParser.SafeConfigParser()
 
     def update_log_config(self):
-        with open(self.filename) as f:
+        with open(self.filepath) as f:
             update_log_config_from_string(f.read())
     
     def set_levels(self, logger_levels):
@@ -48,7 +48,7 @@ class LogConf(object):
         logger_levels is a sequence of (logger, level) pairs,
         where logger is a string naming a logger and level is a number
         """
-        self.parser.read(self.filename)
+        self.parser.read(self.filepath)
         changed = False
         for logger, level in logger_levels.iteritems():
             if logger == "":
@@ -59,21 +59,21 @@ class LogConf(object):
                 self.parser.set(sec_name, 'level', level)
                 changed = True
         if changed:
-            with open(self.filename, 'w') as f:
+            with open(self.filepath, 'w') as f:
                 self.parser.write(f)
 
 class LogConfPub(object):
-    """ Publishes changes in the file named by `filename` to all the
+    """ Publishes changes in the file named by `filepath` to all the
     objects in subscribers. These must have a .send method which will
     be called to send the new file contents to them.
     """
-    def __init__(self, filename, subscribers):
-        self.filename = filename
+    def __init__(self, filepath, subscribers):
+        self.filepath = filepath
         self.subscribers = subscribers
-        util.FileWatcher(self.filename, self.publish_new_file).start()
+        util.FileWatcher(self.filepath, self.publish_new_file).start()
 
     def publish_new_file(self):
-        with open(self.filename) as f:
+        with open(self.filepath) as f:
             data = f.read()
             for s in self.subscribers:
                 s.send(data)
