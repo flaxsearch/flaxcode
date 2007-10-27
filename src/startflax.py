b8c661bdaa94d759ead34ca51537bb5f9d12e533
paul.x.rudin <paul.x.rudin@07824224-6132-0410-9051-db7a3662f6e8>
2007-10-27 07:03:58 +0000

    (Inadvertently ommitted from last checkin)

--- src/startflax.py
+++ src/startflax.py
@@ -127,7 +127,7 @@ class FlaxMain():
         webserver_logconfio = processing.Pipe()
         index_server = indexer.IndexServer(flaxpaths.paths.logconf_path)
         logclient.LogConfPub(flaxpaths.paths.logconf_path,
-                             [webserver_logconfio[0], index_server.logconfio[0]])
+                             [webserver_logconfio[0], index_server.log_config_listener()])
         logclient.LogListener(webserver_logconfio[1]).start()
         logclient.LogConf(flaxpaths.paths.logconf_path).update_log_config()
         flax.options = persist.read_flax(flaxpaths.paths.flaxstate_path)
