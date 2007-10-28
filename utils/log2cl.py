9fe5d3db52083c17cb84ccda157c9a2ad391d605
boulton.rj <boulton.rj@07824224-6132-0410-9051-db7a3662f6e8>
2007-10-28 13:09:57 +0000

    Tidy whitespace.

--- utils/log2cl.py
+++ utils/log2cl.py
@@ -11,7 +11,7 @@
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License along
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
@@ -153,7 +153,7 @@ class LogEntry(object):
                     initial_indent = u'\t  '
                 wrapper = LogTextWrapper(initial_indent=initial_indent)
                 paras[i] = wrapper.fill(paras[i])
-                                        
+
             result.append(u'\n\n'.join(paras))
             result.append(u'\n')
 
@@ -502,7 +502,7 @@ class ChangeLogGetter(LogGetter):
 
 def guess_getter(authors):
     """Guess which revision control system we're in.
-    
+
     Returns an appropriate getter.
 
     """
