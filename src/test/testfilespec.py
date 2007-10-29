f793915c54418fd7e53386c0bf0c964236e9759e
paul.x.rudin <paul.x.rudin@07824224-6132-0410-9051-db7a3662f6e8>
2007-10-29 16:38:34 +0000

    Brought into line with current code.

--- src/test/testfilespec.py
+++ src/test/testfilespec.py
@@ -3,32 +3,29 @@ import unittest
 import os
 import shutil
 import sys
+import tempfile
+
 sys.path = [os.path.normpath(os.path.join(__file__, '..', '..'))]+sys.path
 import filespec
 
 
 class TestFileSpec(unittest.TestCase):
 
-    dirname = "/tmp/testfilespecdir/"
-
     def setUp(self):
         subdirs = ['foo', 'bar']
-        if os.path.exists(self.dirname):
-            shutil.rmtree(self.dirname)
-        os.mkdir(self.dirname)
+        self.dirname = tempfile.mkdtemp()
         for dir in subdirs:
             dname = os.path.join(self.dirname, dir)
             os.mkdir(dname)
             with open(os.path.join(dname, "foo.txt"), 'w') as f:
                 f.write("Hello World")
-                        
-            
         
     def testbasic(self):
-        fs = filespec.FileSpec([self.dirname])
-        print list(fs.files())
+        fs = filespec.FileSpec()
+        fs.update(paths = [self.dirname], formats = ['txt'])
+        self.assertEqual(set(fs.files()),
+                         set((os.path.join(self.dirname, 'foo', 'foo.txt'),
+                              os.path.join(self.dirname, 'bar', 'foo.txt'))))
 
-
-        
 if __name__ == '__main__':
     unittest.main()
