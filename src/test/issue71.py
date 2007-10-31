5c0289c17ad762483d4dffaa94e8366d12bfff65
paul.x.rudin@gmail.com <paul.x.rudin@gmail.com@07824224-6132-0410-9051-db7a3662f6e8>
2007-10-31 18:18:02 +0000

    More simple experiments - suggesting that the problem might be specifically to do with the pdf IFilter.

--- src/test/issue71.py
+++ src/test/issue71.py
@@ -46,3 +46,39 @@ def repeat_file_filter():
             print "no server"
             break
 
+
+# This one is similar to the above, but runs the filter in-process.
+# Memory use also creeps up with this, suggesting that the problem is
+# specifically to do with the ifilter_filter, and not the interprocess
+# stuff.
+
+def in_process_repeat_file_filter():
+    fname = os.path.join(os.path.realpath('sampledocs'), 'ukpga_20060041_en.pdf')
+    while 1:
+        stuff = list(w32com_ifilter.ifilter_filter(fname))
+
+
+# This just repeatedly binds the IFilter, without actually using it.
+# Interestingly memory use goes up much faster with this than the
+# examples above.
+def repeat_bind_ifilter():
+    fname = os.path.join(os.path.realpath('sampledocs'), 'ukpga_20060041_en.pdf')
+    while 1:
+        f, s = w32com_ifilter.get_ifilter_for_file(fname)
+
+# A pdf isn't structured storage, so this is a simplified version of
+# the above, and indeed shows the same growing memory use.
+def repeat_bind_ifilter2():
+    fname = os.path.join(os.path.realpath('sampledocs'), 'ukpga_20060041_en.pdf')
+    while 1:
+        f = w32com_ifilter.load_ifilter(fname)
+
+# Perhaps it's a problem specifically with the pdf filtering thing?
+# This is the same as the above but we use an html file.  Memory use
+# for this does not seem to increase - maybe it's all Adobe's fault?
+# :/
+def repeat_bind_ifilter3():
+    fname = os.path.join(os.path.realpath('sampledocs'), 'Ukpga_19880002_en_1.htm')
+    while 1:
+        f = w32com_ifilter.load_ifilter(fname)
+
