a0c8dc9385b954883cab4c3b941d90dbd0bddb72
paul.x.rudin <paul.x.rudin@07824224-6132-0410-9051-db7a3662f6e8>
2007-11-05 08:24:20 +0000

    Added a test for issue 72

---
+++ src/test/issue72.py
@@ -0,0 +1,32 @@
+import unittest
+import os
+import sys
+import tempfile
+
+sys.path = [os.path.normpath(os.path.join(__file__, '..', '..'))]+sys.path
+import setuppaths
+sys.path = [os.path.normpath(os.path.join(__file__, '..', '..', 'indexserver'))]+sys.path
+import indexer
+import flax
+import xappy
+import flaxpaths
+
+class TestFileTypes(unittest.TestCase):
+
+    def setUp(self):
+        flax.options = flax.make_options()
+        flaxpaths.paths.dbs_dir = tempfile.mkdtemp()
+        self.col = flax.options.collections.new_collection('foo')
+        self.col.update(paths = ['sampledocs/issue72'], formats = ['htm', 'html'])
+        self.indexserver = indexer.IndexServer()
+        self.indexserver.do_indexing(self.col)
+
+    def testFileTypeSearch(self):
+        conn = xappy.SearchConnection(self.col.dbpath())
+        res = conn.search(conn.query_parse('filetype:htm'), 0 , 10)
+        results = [r for r in res]
+        self.assertEqual(len(results), 1)
+
+if __name__ == '__main__':
+    unittest.main()
+
