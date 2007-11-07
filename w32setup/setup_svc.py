a0a6435bcdf9ffc6f65e5b136ba262a9c4503a32
boulton.rj <boulton.rj@07824224-6132-0410-9051-db7a3662f6e8>
2007-11-07 14:32:52 +0000

    Add the MochiKit directory.

--- w32setup/setup_svc.py
+++ w32setup/setup_svc.py
@@ -258,6 +258,8 @@ setup(
                 glob.glob('../src/static/img/*.ico')),
                 ("static/js",
                 glob.glob('../src/static/js/*.js')),
+                ("static/js/MochiKit",
+                glob.glob('../src/static/js/MochiKit/*.js')),
                 
                 ],
 
