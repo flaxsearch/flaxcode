f8953eea6e6a6642dd502a6ea6e4cd1e496369f5
charliejuggler <charliejuggler@07824224-6132-0410-9051-db7a3662f6e8>
2007-11-15 15:17:57 +0000

    way of sleeping that works on Win2k upwards; better error handling

--- w32setup/startflaxservice.bat
+++ w32setup/startflaxservice.bat
@@ -1,8 +1,20 @@
 @echo off
 echo Installing and starting Flax service...
 flaxservice -install -auto
+if errorlevel = 0 goto next
+goto error
+:next
 net start flaxservice
-sleep 3
+if errorlevel = 0 goto next2
+goto error
+:next2
+rem this is a way of waiting for 3 seconds on Windows 2000 upwards
+@ping 127.0.0.1 -n 2 -w 1000 > nul
+@ping 127.0.0.1 -n 3 -w 1000> nul
+
 echo ..started
+exit
+:error
+echo ERROR! Could not start Flax service!
 
 
--- w32setup/stopflaxservice.bat
+++ w32setup/stopflaxservice.bat
@@ -1,9 +1,19 @@
 @echo off
 echo Stopping and removing Flax service...
 net stop flaxservice
+if errorlevel = 0 goto next
+goto error
+:next
 echo Removing Flax service...
 flaxservice -remove
-sleep 3
+if errorlevel = 0 goto next2
+goto error
+:next2
+rem this is a way of waiting for 3 seconds on Windows 2000 upwards
+@ping 127.0.0.1 -n 2 -w 1000 > nul
+@ping 127.0.0.1 -n 3 -w 1000> nul
+
 echo ..removed
-
-
+exit
+:error
+echo ERROR! Could not stop and remove Flax service!
