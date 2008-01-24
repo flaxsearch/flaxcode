d6f11c8fa3d313c4a135375b16b1c4051ca96869
charliejuggler <charliejuggler@07824224-6132-0410-9051-db7a3662f6e8>
2008-01-24 14:57:32 +0000

    make sure distutils works when compiling 'processing' module

--- w32setup/buildit.bat
+++ w32setup/buildit.bat
@@ -1,5 +1,8 @@
 rem A simple batch file to ease building a fresh version of Flax
 cd ..
+rem These are necessary to force Distutils to use Visual C++ Express Edition and the Platform SDK
+set DISTUTILS_USE_SDK=1
+set MSSDK=1
 python utils/install_dependencies.py
 cd w32setup
 rd dist /s /q
