56cd93ea8b2b1bd1c49134a293ef687317316434
charliejuggler <charliejuggler@07824224-6132-0410-9051-db7a3662f6e8>
2008-01-24 15:26:44 +0000

    set up Visual C++ environment

--- w32setup/buildit.bat
+++ w32setup/buildit.bat
@@ -1,8 +1,11 @@
 rem A simple batch file to ease building a fresh version of Flax
-cd ..
+rem .
+rem first make sure the Visual C++ environment is set up correctly 
+call setupvc.bat
 rem These are necessary to force Distutils to use Visual C++ Express Edition and the Platform SDK
 set DISTUTILS_USE_SDK=1
 set MSSDK=1
+cd ..
 python utils/install_dependencies.py
 cd w32setup
 rd dist /s /q
