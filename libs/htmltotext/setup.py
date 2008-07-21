52c17206ab75107f8e4400ccacc7937ce20d4a9b
boulton.rj <boulton.rj@07824224-6132-0410-9051-db7a3662f6e8>
2008-07-21 07:31:23 +0000

    setup.py: Update for a new release (0.7).

--- libs/htmltotext/setup.py
+++ libs/htmltotext/setup.py
@@ -60,13 +60,13 @@ that library).
 """
 
 setup(name = "htmltotext",
-      version = "0.6",
+      version = "0.7",
       author = "Richard Boulton",
       author_email = "richard@lemurconsulting.com",
       maintainer = "Richard Boulton",
       maintainer_email = "richard@lemurconsulting.com",
       url = "http://code.google.com/p/flaxcode/wiki/HtmlToText",
-      download_url = "http://flaxcode.googlecode.com/files/htmltotext-0.6.tar.gz",
+      download_url = "http://flaxcode.googlecode.com/files/htmltotext-0.7.tar.gz",
       description = "Extract text and some metainfo from HTML, coping with malformed pages as well as possible.",
       long_description = long_description,
       classifiers = [
