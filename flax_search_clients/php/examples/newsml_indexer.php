d30611c5888b0c4e320f5bd0991ed2356bf520c2
banoffi <banoffi@07824224-6132-0410-9051-db7a3662f6e8>
2009-05-01 16:42:39 +0000

    * examples/newsml_indexer.php: Started example NewsML indexer.

---
+++ flax_search_clients/php/examples/newsml_indexer.php
@@ -0,0 +1,82 @@
+<?php 
+
+require_once('../flaxclient.php');
+
+$fieldname = $docdata = $doc = null;
+$current_file = null;
+
+function contents($parser, $data){
+    global $fieldname, $docdata;
+    
+    if ($fieldname) {
+        $docdata .= $data;
+    }
+} 
+
+function startTag($parser, $name, $attrs){ 
+    global $fieldname, $docdata, $doc, $current_file;
+
+    if ($name == 'NEWSITEM') {
+        $doc = array('_PATH' => $current_file);
+    } 
+    else if ($name == 'HEADLINE' || $name == 'BYLINE' || 
+               $name == 'DATELINE' || $name == 'TEXT') {
+        $docdata = '';
+        $fieldname = $name;
+    }
+} 
+
+function endTag($parser, $name){ 
+    global $fieldname, $docdata, $doc;
+
+    if ($name == 'NEWSITEM') {
+        add_document();
+    } 
+    else if ($name == 'HEADLINE' || $name == 'BYLINE' || 
+               $name == 'DATELINE' || $name == 'TEXT') {
+        $doc[$fieldname] = $docdata;        
+        $fieldname = null;
+    }
+} 
+
+function add_document() {
+    global $doc, $flaxdb;
+#    $doc['TEXT'] = 'foo';
+    print_r($doc);
+    $flaxdb->addDocument($doc);
+}
+
+$flax = new FlaxSearchService(new FlaxRestClient($argv[1]));
+$flaxdb = $flax->createDatabase('newsml');
+
+$flaxdb->addField('HEADLINE', array('store' => true, 
+    'freetext' => array('language' => 'en', 'term_frequency_multiplier' => 3)));
+
+$flaxdb->addField('BYLINE', array('store' => true, 'exacttext' => true));
+$flaxdb->addField('DATELINE', array('store' => true, 'exacttext' => true));
+$flaxdb->addField('TEXT', array('store' => false, 'freetext' => array('language' => 'en')));
+$flaxdb->addField('_PATH', array('store' => true));
+$flaxdb->commit();
+
+$dir_handle = opendir($argv[2]);
+while ($file = readdir($dir_handle)) {
+    if (preg_match('/\.xml$/', $file)) {
+        print "$file \n";
+        $current_file = "${argv[2]}/${file}";
+        $fp = fopen($current_file, "r"); 
+        $data = fread($fp, 1000000); 
+
+        $xml_parser = xml_parser_create(); 
+        xml_set_element_handler($xml_parser, "startTag", "endTag"); 
+        xml_set_character_data_handler($xml_parser, "contents"); 
+
+        if(!(xml_parse($xml_parser, $data, false))){ 
+            print("Error on line " . xml_get_current_line_number($xml_parser)."\n"); 
+        }         
+        xml_parser_free($xml_parser);
+    }
+}
+
+fclose($fp); 
+
+?>
