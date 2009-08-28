c531cadd651a31acfdc3abf23d3b88db8ae007d5
banoffi <banoffi@07824224-6132-0410-9051-db7a3662f6e8>
2009-08-28 13:57:39 +0000

    * tutorial/flax_search_service_tutorial.txt, * tutorial/python/t6_search.py, * tutorial/python/t7_search_author.py: Added search bit to tutorial and   made the scripts report "no results" in a more helpful way.

--- flax_search_service/docs/tutorial/python/t7_search_author.py
+++ flax_search_service/docs/tutorial/python/t7_search_author.py
@@ -10,11 +10,14 @@ db = conn.db('books')
 # do the search and get the results
 results = db.search_structured(filter=['author:%s' % sys.argv[1]])
 
-# print a summary of the results
-print '%d to %d of %s results' % (results.start_rank + 1, 
-    results.end_rank,
-    results.matches_human_readable_estimate)
-
-# print the rank and title of each result
-for hit in results.results:
-    print '%s. %s' % (hit.rank + 1, hit.data['title'][0])
+if results.results:
+    # print a summary of the results
+    print '%d to %d of %s results:' % (results.start_rank + 1, 
+        results.end_rank,
+        results.matches_human_readable_estimate)
+    
+    # print the rank and title of each result
+    for hit in results.results:
+        print '(%s) %s' % (hit.rank + 1, hit.data['title'][0])
+else:
+    print 'no results'
