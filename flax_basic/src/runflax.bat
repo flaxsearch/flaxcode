dace56883f1041914fb182660399ad9103e89f37
charliejuggler <charliejuggler@07824224-6132-0410-9051-db7a3662f6e8>
2009-09-03 09:48:28 +0000

    echo off to stop comments being printed

--- flax_basic/src/runflax.bat
+++ flax_basic/src/runflax.bat
@@ -1,8 +1,9 @@
+@Echo off
 rem This file is a quick way to start Flax Basic from the command line on Windows. To run it:
 rem      1. First make sure you have Python on your PATH 
 rem      2. If you already have Flax Basic installed as a Service, you'll need to halt it using 'net stop flaxservice'
 rem      3. Then run this file with the command line option --set-admin-password and follow the prompts
 rem      4. Then run this file again, and start a browser and navigate to http://localhost:8090
 rem      5. CTRL-C should stop the process
-
+@echo on
 python startflax.py --main-dir=data --conf-dir=. --dbs-dir=data\dbs --log-dir=data\logs --var-dir=data\var --src-dir=. %1 %2 %3 %4
