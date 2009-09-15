3866c32dce37863154c03642bd6ea031b4d41e69
paul.x.rudin@gmail.com <paul.x.rudin@gmail.com@07824224-6132-0410-9051-db7a3662f6e8>
2009-09-15 10:37:29 +0000

    updated the test for ghostscript - we don't need for it to be on the path as ImageMagick locates it via the registry... so we just try to open a test pdf.

--- flax_basic/libs/previewgen/previewgen/test/dependencies.py
+++ flax_basic/libs/previewgen/previewgen/test/dependencies.py
@@ -13,6 +13,7 @@ import os
 
 
 dirname = os.path.dirname(os.path.abspath(__file__))
+datapath = os.path.join(dirname, "data")
 parent = os.path.abspath(os.path.dirname(dirname))
 sys.path = [parent] + sys.path
 
@@ -29,8 +30,15 @@ class DependeciesTestCase(unittest.TestCase):
     def test_magicwand_import(self):
         import PythonMagickWand
 
-    def test_ghostscript_present(self):
-        subprocess.check_call(["ghostscript", "-v"])
+    def test_ghostscript_found(self):
+        """ ImageMagick uses ghostscript to read pdfs - this test
+        checks that it can locate ghostscipt.
+
+        """
+        import PythonMagickWand
+        wand = PythonMagickWand.NewMagickWand()
+        doc1 = os.path.join(datapath, "doc1.pdf")
+        PythonMagickWand.MagickReadImage(wand, doc1)
         
 if __name__ == "__main__":
     unittest.main()
